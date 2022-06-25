ec970db04b5319e5edf87fb9112c8fae555a102a
0Island0 <95268649+0Island0@users.noreply.github.com>
2022-06-25 17:26:47 +0800

    Update 4-6.c

--- 4-6.c
+++ 4-6.c
@@ -2,9 +2,9 @@
 int main()
 {
 	char ming[40], xing[40];
-	printf("«Î ‰»Î√˚£∫");
+	printf("please enter your ming:");
 	scanf("%s", ming);
-	printf("«Î ‰»Î–’£∫");
+	printf("please enter your xing:");
 	scanf("%s", xing);
 	printf("%s %s\n", ming, xing);
 	printf("%*d %*d\n", strlen(ming), strlen(ming), strlen(xing), strlen(xing));
