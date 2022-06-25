0242b3e902d52511d5f392f140c735c4e5fa84ab
0Island0 <95268649+0Island0@users.noreply.github.com>
2022-06-25 17:14:27 +0800

    Update 4-3.c

--- 4-3.c
+++ 4-3.c
@@ -2,7 +2,7 @@
 int main()
 {
 	float a;
-	printf("请输入一个浮点数：\n");
+	printf("please enter a num\n");
 	scanf("%f", &a);
 	printf("The input is %.1f or %.1e\n", a, a);
 	printf("The input is %.3f or %.3e\n", a, a);
