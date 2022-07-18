48cd8421ce043b7a99bc40b49322a7f069a6bab9
0Island0 <95268649+0Island0@users.noreply.github.com>
2022-07-18 21:58:47 +0800

    Update 7-7.c

--- 7-7.c
+++ 7-7.c
@@ -1,34 +1,38 @@
 #include<stdio.h>
-#define A 10
+#define A 10.00
 #define B 40
 int main()
 {
-	double m;
-	double h;
-	printf("Enter your workking time(h):");
-	scanf("%lf", &h);
-	if (h <= 40)
+	double h, sui, sum;
+	printf("enter the hours:");
+	while ((scanf("%lf", &h)) == 1 && h>0)
 	{
-		m = h * A;
+		if (h < B)
+		{
+			sum = A * h;
+		}
+		else
+		{
+			h = B + (h - B) * 1.5;
+			sum = A * h;
+		}
+		if (sum <= 300)
+		{
+			sui = sum * 0.15;
+		}
+		else if (sum > 300 & sum < 450)
+		{
+			sui = 300 * 0.15 - (sum - 300) * 0.2;
+		}
+		else
+		{
+			sui = 300 * 0.15 - 150 * 0.2 - (sum - 450) * 0.25;
+		}
+		printf("%lf,%lf,%lf\n", sum, sui, sum-sui);
+		printf("enter the hours again(q to quit):");
 	}
-	else
-	{
-		m = 40 * A + (h - B) * A *1.5;
-	}
-	printf("税前%f\n", m);
-
-	if (m <= 300)
-	{
-		m -= m*0.15;
-	}
-	else if (m > 300 && m <= 450)
-	{
-		m -= 300 * 0.15 + (m - 300) * 0.2;
-	}
-	else
-	{
-		m -= 300 * 0.15 + 150 * 0.2 + (m - 450) * 0.25;
-	}
-	printf("税后%lf", m);
+	printf("done");
+	
+	
 	return 0;
 }
